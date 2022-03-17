ca4f416350d8c24517beeb96c01fc81db465c136
GitHub Action <action@github.com>
2022-03-17 07:06:00 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zettianmaximus/generatetestcase/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zettianmaximus.generatetestcase.services
+
+import com.github.zettianmaximus.generatetestcase.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
