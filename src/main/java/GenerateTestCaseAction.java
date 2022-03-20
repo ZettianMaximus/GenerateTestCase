bdd203c6ced7b61e073088147167caf46907eedf
Christina Celestino <christinacx7@gmail.com>
2022-03-19 22:53:17 -0500

    Edited Action performed in GenerateTestCaseAction class. When a word is highlighted and "Generate Test Case" is selected, print the highlighted text to the console.

--- src/main/java/GenerateTestCaseAction.java
+++ src/main/java/GenerateTestCaseAction.java
@@ -1,11 +1,20 @@
 import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
 
 public class GenerateTestCaseAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         //BrowserUtil.browse("https://stackoverflow.com/questions/ask");
         //code here
+
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        CaretModel caretModel = editor.getCaretModel();
+        String selectedText = caretModel.getCurrentCaret().getSelectedText();
+
+        System.out.println(selectedText);
     }
 }
